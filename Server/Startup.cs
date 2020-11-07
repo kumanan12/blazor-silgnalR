47d0300b84da2541f7d2f5ebcfe5d762558a5ace
Kumanan Murugesan <kumanan12@gmail.com>
2020-11-07 11:07:59 -0500

    Signalr communication working

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.Linq;
+using BlazorSignalR.Server.Hubs;
 
 namespace BlazorSignalR.Server
 {
@@ -22,8 +23,12 @@ namespace BlazorSignalR.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddSignalR();
             services.AddControllersWithViews();
+            services.AddResponseCompression(opts =>
+            {
+                opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new[] { "application/octet-stream" });
+            });
             services.AddRazorPages();
         }
 
@@ -52,6 +57,7 @@ namespace BlazorSignalR.Server
             {
                 endpoints.MapRazorPages();
                 endpoints.MapControllers();
+                endpoints.MapHub<ChatHub>("/chathub");
                 endpoints.MapFallbackToFile("index.html");
             });
         }
